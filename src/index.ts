da653ac72f822851138d7576b20c20784d313fbb
xDaoud <moshdawd@gmail.com>
2026-02-26 23:45:18 +0200

    feat: add /healthz readiness endpoint and serve static files at /app

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import express from "express";
+import { Request, Response } from "express";
 
 const app = express();
 const PORT = 8080;
 
-app.use(express.static("."));
+function handlerReadiness(req: Request, res: Response){
+    res.set('Content-Type', 'text/plain; charset=utf-8');
+    res.send("OK")
+}
+app.get("/healthz", handlerReadiness);
+app.use("/app", express.static("./app"));
+app.use("/app", express.static("./app/assets"));
 
 app.listen(PORT, () => {
   console.log(`Server is running at http://localhost:${PORT}`);
