ca1c4ec66673ee2f149fcc1a68f7bde729a43c23
xDaoud <moshdawd@gmail.com>
2026-02-27 21:29:49 +0200

    feat: add chirp validation endpoint with JSON body parsing

--- src/index.ts
+++ src/index.ts
@@ -13,11 +13,11 @@ function middlewareMetricsInc(req: Request, res: Response, next: NextFunction) {
 function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
 	res.on("finish", () => {
 		const status = res.statusCode;
-		if(status !== 200){
+		if (status !== 200) {
 			console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${status}`);
 		}
 	});
-	next();	
+	next();
 }
 app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc);
@@ -25,7 +25,7 @@ async function handlerReadiness(req: Request, res: Response) {
 	res.set('Content-Type', 'text/plain; charset=utf-8');
 	res.send("OK");
 }
-async function handlerMetrics(req: Request, res: Response){
+async function handlerMetrics(req: Request, res: Response) {
 	res.set('Content-Type', 'text/html; charset=utf-8');
 	res.send(`<html>
   <body>
@@ -36,9 +36,48 @@ async function handlerMetrics(req: Request, res: Response){
 }
 async function handlerReset(req: Request, res: Response) {
 	config.fileserverHits = 0;
-	res.send();	
+	res.send();
 }
+async function handlerValidate(req: Request, res: Response) {
+	let body = "";
+	req.on("data", (chunk) => {
+		body += chunk;
+	});
+	req.on("end", () => {
+		try {
+			const parsedBody = JSON.parse(body);
+			if (!parsedBody.body) {
+				throw new Error("Something went wrong");
+			}
+			if (parsedBody.body.length > 140) {
+				throw new Error("Chirp is too long")
+			}
+			type responseData = {
+				valid: boolean,
+			}
+			const respBody: responseData = {
+				valid: true,
+			};
 
+			res.header("Content-Type", "application/json");
+			res.status(200).send(JSON.stringify(respBody));
+		} catch (error) {
+			type caught = {
+				error: string;
+			}
+			const resJson: caught = {
+				error: "Something went wrong",
+			}
+			if (error instanceof Error) {
+				resJson.error = error.message;
+			}
+			res.header("Content-Type", "application/json");
+			res.status(400).send(JSON.stringify(resJson));
+		}
+	});
+
+}
+app.post("/api/validate_chirp", handlerValidate);
 app.post("/admin/reset", handlerReset);
 app.get("/admin/metrics", handlerMetrics);
 app.get("/api/healthz", handlerReadiness);
