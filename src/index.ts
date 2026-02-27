65aef12be7b5bd244e63f8199466b55e26af05a1
xDaoud <moshdawd@gmail.com>
2026-02-27 22:58:51 +0200

    refactor: replace manual body parsing with express.json() middleware

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express, { NextFunction } from "express";
 import { Request, Response } from "express";
 import { config } from "./config.js";
+import { listenerCount } from "node:process";
 
 const app = express();
 const PORT = 8080;
@@ -19,6 +20,7 @@ function middlewareLogResponses(req: Request, res: Response, next: NextFunction)
 	});
 	next();
 }
+app.use(express.json());
 app.use(middlewareLogResponses);
 app.use("/app", middlewareMetricsInc);
 async function handlerReadiness(req: Request, res: Response) {
@@ -39,44 +41,29 @@ async function handlerReset(req: Request, res: Response) {
 	res.send();
 }
 async function handlerValidate(req: Request, res: Response) {
-	let body = "";
-	req.on("data", (chunk) => {
-		body += chunk;
-	});
-	req.on("end", () => {
-		try {
-			const parsedBody = JSON.parse(body);
-			if (!parsedBody.body) {
-				throw new Error("Something went wrong");
-			}
-			if (parsedBody.body.length > 140) {
-				throw new Error("Chirp is too long")
-			}
-			type responseData = {
-				valid: boolean,
-			}
-			const respBody: responseData = {
-				valid: true,
-			};
-
-			res.header("Content-Type", "application/json");
-			res.status(200).send(JSON.stringify(respBody));
-		} catch (error) {
-			type caught = {
-				error: string;
-			}
-			const resJson: caught = {
-				error: "Something went wrong",
-			}
-			if (error instanceof Error) {
-				resJson.error = error.message;
-			}
-			res.header("Content-Type", "application/json");
-			res.status(400).send(JSON.stringify(resJson));
+	try {
+		const parsedBody = req.body;
+		if (!parsedBody.body) {
+			throw new Error("Something went wrong");
 		}
-	});
+		if (parsedBody.body.length > 140) {
+			throw new Error("Chirp is too long")
+		}
+		res.header("Content-Type", "application/json");
+		res.status(200).send(JSON.stringify({ valid: true }));
+		
+	} catch (error) {
+		let message = "Something went wrong";
 
+		if (error instanceof Error) {
+			message = error.message;
+		}
+		res.header("Content-Type", "application/json");
+		res.status(400).send(JSON.stringify({ error: message }));
+	}
 }
+
+
 app.post("/api/validate_chirp", handlerValidate);
 app.post("/admin/reset", handlerReset);
 app.get("/admin/metrics", handlerMetrics);
