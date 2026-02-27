6a6095bd1482ff1f6b9452755074a893e66e612a
xDaoud <moshdawd@gmail.com>
2026-02-27 15:26:56 +0200

    feat: Add middlewareLogResponses middleware to log non-OK responses

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,28 @@
-import express from "express";
+import express, { NextFunction } from "express";
 import { Request, Response } from "express";
 
 const app = express();
 const PORT = 8080;
 
-function handlerReadiness(req: Request, res: Response){
-    res.set('Content-Type', 'text/plain; charset=utf-8');
-    res.send("OK")
+function middlewareLogResponses(req: Request, res: Response, next: NextFunction) {
+	res.on("finish", () => {
+		const status = res.statusCode;
+		if(status !== 200){
+			console.log(`[NON-OK] ${req.method} ${req.url} - Status: ${status}`);
+		}
+	});
+	next();	
+}
+app.use(middlewareLogResponses);
+
+function handlerReadiness(req: Request, res: Response) {
+	res.set('Content-Type', 'text/plain; charset=utf-8');
+	res.send("OK")
 }
 app.get("/healthz", handlerReadiness);
 app.use("/app", express.static("./app"));
 app.use("/app", express.static("./app/assets"));
 
 app.listen(PORT, () => {
-  console.log(`Server is running at http://localhost:${PORT}`);
+	console.log(`Server is running at http://localhost:${PORT}`);
 });
